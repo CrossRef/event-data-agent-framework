5e8e4c1347667da317c7daa1548d8bf22bd0f2bc
Joe Wass <joe@afandian.com>
2016-09-13 16:55:41 +0100

    Add license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.crossref.event-data-agent-framework "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Framework for writing Crossref Event Data agents."
+  :url "http://eventdata.crossref.org"
+  :license {:name "The MIT License (MIT)"
+            :url "https://opensource.org/licenses/MIT"}
   :plugins [[lein-localrepo "0.5.3"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [yogthos/config "0.8"]
